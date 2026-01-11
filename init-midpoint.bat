83e053d1eac216b6871928665998255104a7c07d
VeronikaDoschenko <33316274+VeronikaDoschenko@users.noreply.github.com>
2026-01-11 22:33:11 +0300

    Add files via upload

---
+++ init-midpoint.bat
@@ -0,0 +1,15 @@
+@echo off
+echo init Tables...
+
+
+docker cp .\midpoint-dist\doc\config\sql\native\postgres.sql dostup_claster_docker-postgres-1:/tmp/postgres.sql
+docker cp .\midpoint-dist\doc\config\sql\native\postgres-audit.sql dostup_claster_docker-postgres-1:/tmp/postgres-audit.sql
+docker cp .\midpoint-dist\doc\config\sql\native\postgres-quartz.sql dostup_claster_docker-postgres-1:/tmp/postgres-quartz.sql
+
+
+docker compose exec postgres psql -U midpoint_user_cluster -d midpoint_cluster -f /tmp/postgres.sql
+docker compose exec postgres psql -U midpoint_user_cluster -d midpoint_cluster -f /tmp/postgres-audit.sql
+docker compose exec postgres psql -U midpoint_user_cluster -d midpoint_cluster -f /tmp/postgres-quartz.sql
+
+echo Ready!
+pause
